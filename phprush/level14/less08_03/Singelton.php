2b0781affe3d0aa267bd3c6fb8950ad05e3cb2b6
sydorenkovd <sydorenkovd@gmail.com>
2016-02-15 21:20:12 +0200

    level14less08_03

---
+++ phprush/level14/less08_03/Singelton.php
@@ -0,0 +1,36 @@
+<?php
+
+/* Singleton
+Класс является синглтоном (реализует паттерн(шаблон) Singleton), если позволяет создать всего один объект своего типа.
+
+Реализовать Singleton pattern:
+1. Создай класс Singleton в отдельном файле.
+2. Добавь в него статический метод getInstance().
+3. Метод getInstance должен возвращать один и тот же объект класса Singleton при любом вызове метода getInstance.
+4. Подумай, каким образом можно запретить создание других объектов этого класса.
+5. Сделай все конструкторы в классе Singleton приватными (private).
+
+6. В итоге должна быть возможность создать объект (экземпляр класса) ТОЛЬКО используя метод getInstance.
+*/
+
+class Singleton
+{
+    private static $instance;
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+            echo 1;
+            return self::$instance;
+        }
+    }
+}
+
+Singleton::getInstance();
+Singleton::getInstance();
+Singleton::getInstance();
