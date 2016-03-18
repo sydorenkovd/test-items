cc1b28999348cb0f0aac2c2b577f481bc602219a
sydorenkovd <sydorenkovd@gmail.com>
2016-03-18 22:15:08 +0200

    banner request and save into database

---
+++ testPHPDev/banner.php
@@ -0,0 +1,10 @@
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$time = $_SERVER['REQUEST_TIME'];
+$user_agent = $_SERVER['HTTP_USER_AGENT'];
+
+$db = new PDO('mysql:host=localhost;port=3306;dbname=banner', $user, $password);
+$st = $db->prepare('INSERT INTO banner (ip, time, user_agent, banner_show) VALUES (?,?,?,?)');
+$st->execute([$ip, $time, $user_agent, $banner_show]);
+?>
+<img src="banner.php" alt=ФЅаннерФ border=Ф0Ф />
