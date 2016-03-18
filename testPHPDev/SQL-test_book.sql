6cb8790973ec28e5a05708741e44cb0897ff0ae0
sydorenkovd <sydorenkovd@gmail.com>
2016-03-18 22:38:37 +0200

    sql task first part

---
+++ testPHPDev/SQL-test_book.sql
@@ -0,0 +1,9 @@
+--
+-- База данных: `jobbase`
+-- it's the same thing as in task
+--
+select title, count(tbl_tag.id) as numberOfTags
+from `tbl_tag`, `tbl_tagPost`, `tbl_posts`
+where tbl_posts.id = tbl_tagPost.post_id and tbl_tag.id = tbl_tagPost.tag_id
+and tbl_posts.is_moderate = 1
+group by numberOfTags;
